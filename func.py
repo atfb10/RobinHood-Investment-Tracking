018522df8eec3fdde31e7866cddd58542bbad772
atfb84 <atfb84@mst.edu>
2023-03-12 15:31:38 -0700

    RobinHood Code Blocker

--- func.py
+++ func.py
@@ -3,7 +3,7 @@ Author: Adam Forestier
 Date: March 12, 2023
 Description: func.py contains the logic behind the running of the application
 '''
-import smtplib
+import functools, robin_stocks, smtplib
 
 from credentials import (
     ROBINHODD_USERNAME,
@@ -14,7 +14,8 @@ def send_timely_mail(connection: smtplib.SMTP, email: dict) -> None:
     '''
     argument: SMTP CONNECTION. EmailMessage() object
     returns: None
-    Description: send_timely_mail sends an email with share purchase price and its current status at the time the function runs
+    Description: send_timely_mail sends an email with share purchase price and its current status at the time the function runs.
+    It then kills the connection to the google server
     '''
     connection.send_message(email)
     connection.quit()
@@ -22,14 +23,12 @@ def send_timely_mail(connection: smtplib.SMTP, email: dict) -> None:
 
 def extract_data() -> dict:
     '''
-    arguments: 
+    arguments: None
     returns: Dictionary of investmentment data
     description: extract data gets the desired data from one's RobinHood account and returns it as a Python dictionary object
     '''
-    data = {}
-
-
-    return data
+    login = robin_stocks.robinhood.login(ROBINHODD_USERNAME, ROBINHOOD_PASSWORD)
+    return robin_stocks.robinhood.build_holdings()
 
 def build_message(data: dict) -> str:
     '''
