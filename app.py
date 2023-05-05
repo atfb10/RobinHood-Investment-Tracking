7d04711e71316d84662a9c5e0a3f84746cf77852
atfb84 <atfb84@mst.edu>
2023-05-05 13:21:33 -0700

    Extract Data and format as data frame

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ Author: Adam Forestier
 Date: May 5, 2023
 Description: app.py manages the running of the application
 '''
+import schedule
 
-import schedule, smtplib
 from pytz import timezone
 
 from email.message import EmailMessage
@@ -13,12 +13,12 @@ from email.message import EmailMessage
 from credentials import (
     EMAIL_FROM,
     EMAIL_FROM_PASSWORD,
-    EMAIL_TO
+    EMAIL_TO,
+    PHONE_NUM,
 )
 from func import (
     build_message,
-    extract_data,
-    send_timely_mail
+    extract_data
 )
 
 # Connect to gmail
@@ -30,15 +30,13 @@ from func import (
 
 # Get Data
 investment_data = extract_data()
-email_content = build_message(investment_data)
+print(investment_data)
 
-# generate email message
-email = EmailMessage()
-email['Subject'] = 'Investing Update'
-email['From'] = EMAIL_FROM
-email['To'] = EMAIL_TO
-email.set_content(email_content)
+# Generate text message
+text_msg = build_message(investment_data)
 
-# Send email when it is time
-schedule.every().monday.at('7:30', timezone('US/Mountain')).do(send_timely_mail(SMTP_CONNECTOR, email))
-schedule.every().friday.at('14:00', timezone('US/Mountain')).do(send_timely_mail(SMTP_CONNECTOR, email))
+
+
+# TODO: Perform Functionality every 5 minutes during week when stock market is open
+# schedule.every().monday.at('7:30', timezone('US/Mountain')).do(send_timely_mail(SMTP_CONNECTOR, email))
+# schedule.every().friday.at('14:00', timezone('US/Mountain')).do(send_timely_mail(SMTP_CONNECTOR, email))
--- func.py
+++ func.py
@@ -3,23 +3,13 @@ Author: Adam Forestier
 Date: May 5, 2023
 Description: func.py contains the logic behind the running of the application
 '''
-import functools, robin_stocks, smtplib
+import pandas as pd
+import functools, robin_stocks
 
 from credentials import (
     ROBINHODD_USERNAME,
     ROBINHOOD_PASSWORD
 )
-
-def send_timely_mail(connection: smtplib.SMTP, email: dict) -> None:
-    '''
-    argument: SMTP CONNECTION. EmailMessage() object
-    returns: None
-    Description: send_timely_mail sends an email with share purchase price and its current status at the time the function runs.
-    It then kills the connection to the google server
-    '''
-    connection.send_message(email)
-    connection.quit()
-    return
 
 def extract_data() -> dict:
     '''
@@ -28,7 +18,7 @@ def extract_data() -> dict:
     description: extract data gets the desired data from one's RobinHood account and returns it as a Python dictionary object
     '''
     robin_stocks.robinhood.login(ROBINHODD_USERNAME, ROBINHOOD_PASSWORD)
-    return robin_stocks.robinhood.build_holdings()
+    return pd.DataFrame(robin_stocks.robinhood.build_holdings()).transpose().reset_index().rename({'index': 'ticker'}, axis=1)
 
 def build_message(data: dict) -> str:
     '''
