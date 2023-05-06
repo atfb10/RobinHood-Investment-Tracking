b4c149e50335df1f9ca087bcd3bde36dc9aa4e5b
atfb84 <atfb84@mst.edu>
2023-05-05 17:53:41 -0700

    OOP functioning!

--- app.py
+++ app.py
@@ -15,22 +15,19 @@ from credentials import (
     EMAIL_FROM,
     EMAIL_FROM_PASSWORD,
     EMAIL_TO,
-    PHONE_NUM,
     ROBINHODD_USERNAME,
     ROBINHOOD_PASSWORD
 )
 from func import (
     build_message,
     extract_data,
-    send_text
 )
+from user import RobinUser
 
-# Get Data
-investment_data = extract_data(username=ROBINHODD_USERNAME, password=ROBINHOOD_PASSWORD)
-
-# Generate text message
-msg = build_message(df=investment_data)
-
+# Create User
+user = RobinUser(username=ROBINHODD_USERNAME, password=ROBINHOOD_PASSWORD, email=EMAIL_TO)
+print(user.df)
+print(user.directory)
 
 # TODO: Perform Functionality every market close on Fridays
 # schedule.every().friday.at('14:00', timezone('US/Mountain')).do(send_timely_mail(SMTP_CONNECTOR, email))
--- user.py
+++ user.py
@@ -3,23 +3,44 @@ Author: Adam Forestier
 Date: May 5, 2023
 Description: user contains the User class that stock data is gathered for
 '''
-
+from email.mime.multipart import MIMEMultipart
+from email.mime.application import MIMEApplication
+from email.mime.text import MIMEText
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.application import MIMEApplication
+import os
 import pandas as pd
+import robin_stocks
 
-from credentials import PATH_TO_PROJECT
+from credentials import (
+    PATH_TO_PROJECT, 
+    EMAIL_FROM, 
+    EMAIL_FROM_PASSWORD
+)
+
+# Constants
+PORT = 587
+HOST = 'smtp.gmail.com'
+EMAIL_SUBJECT = 'Weekly Stock Update'
+MESSAGE_BODY = '''Howdy!
+
+Attached is your weekly stock update
+
+Best,
+Adam
+'''
 
 class RobinUser:
-
-    def __init__(self, username: str, password: str, email: str, df: pd.DataFrame):
+    def __init__(self, username: str, password: str, email: str):
         '''
-        every mountain project user has a userid and username
+        RobinUser contructor
         '''
         self.username = username
         self.password = username
         self.email = email
-        self.user_tick_url = f'{self.BASE_URL}{userid}/{username}{self.TICK}'
-        self.directory = f'{PATH_TO_PROJECT}/user_files/{username}'
-        self.df = df
+        self.directory = f'{PATH_TO_PROJECT}\\user_files\\{username}'
+        self.df = self.extract_data()
 
     def __repr__(self) -> str:
         '''
@@ -32,3 +53,51 @@ class RobinUser:
         prints the user's id and name 
         '''
         return f'Username: {self.username}. User Email: {self.email}'
+    
+    def extract_data(self) -> pd.DataFrame:
+        '''
+        arguments: None
+        returns: Dictionary of investmentment data
+        description: extract data gets the desired data from one's RobinHood account and returns it as a Python dictionary object
+        '''
+        robin_stocks.robinhood.login(username=self.username, password=self.password)
+        return pd.DataFrame(robin_stocks.robinhood.build_holdings()).transpose().reset_index().rename({'index': 'ticker'}, axis=1)
+
+    def build_message(self) -> str:
+        '''
+        arguments: data. Dataframe of current holdings
+        returns: string that will be the email content
+        description: extract data gets the desired data from one's RobinHood account and returns it as a Python dictionary object
+        '''
+        ticker = self.df.iloc[0]['ticker']
+        equity_change = self.df.iloc[0]['equity_change']
+        investment_status_msg = f'Your current equity change for {ticker} = ${equity_change}'
+
+        return investment_status_msg
+    
+    def send_mail(self) -> None:
+        '''
+        arguments: self
+        returns: None
+        Description: send_email sends an email to user containing a zip file with all user data and graph files
+        '''
+        msg = MIMEMultipart()
+        body_part = MIMEText(MESSAGE_BODY, 'plain')
+        msg['Subject'] = EMAIL_SUBJECT
+        msg['From'] = EMAIL_FROM
+        msg['To'] = self.email
+        msg.attach(body_part)
+        parent_dir = 'user_files/'
+        user_folder = str(self.username)
+        path = os.path.join(parent_dir, user_folder)
+        zip_path = f'{path}.zip'
+        with open(zip_path,'rb') as file:
+            msg.attach(MIMEApplication(file.read(), Name=zip_path))
+
+        smtp_obj = smtplib.SMTP(host=HOST, port=PORT)
+        smtp_obj.starttls()
+        smtp_obj.login(EMAIL_FROM, EMAIL_FROM_PASSWORD)
+
+        smtp_obj.sendmail(msg['From'], msg['To'], msg.as_string())
+        smtp_obj.quit()
+        return
